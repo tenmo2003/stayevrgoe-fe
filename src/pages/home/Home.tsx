72e769644e896f5b09c930439789af9138731799
hothugiang <21020309@vnu.edu.vn>
2024-05-24 00:45:58 +0700

    navbar + profile

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,12 +3,7 @@ import { toast } from "sonner";
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center overflow-auto">
-      <a
-        className="absolute left-3 top-3 rounded-xl bg-green-300 p-3"
-        href="/test"
-      >
-        Go to Test page
-      </a>
+      
       <button
         className="rounded-xl border border-black bg-blue-400 p-3"
         onClick={() => toast("Hello, World!")}
