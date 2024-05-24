b1c3faa92458c2595e777945b7c542ea0587d2a2
hothugiang <21020309@vnu.edu.vn>
2024-05-25 02:18:03 +0700

    login/signup/admin

--- src/navs/NavBar.tsx
+++ src/navs/NavBar.tsx
@@ -1,10 +1,52 @@
 import { Disclosure } from '@headlessui/react'
-import { BellOutlined } from '@ant-design/icons'
+import { BellOutlined, SearchOutlined } from '@ant-design/icons'
 import { Input, Button } from "antd";
 import logo from "../assets/stayevrgoe.png";
 import ava from "../assets/ava.png"
+import { useState } from 'react';
+import LoginModal from '../components/LoginModal';
+import SignupModal from '../components/SignupModal'
 
 const Navbar = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+    const handleLogoutClick = () => {
+        setIsLoggedIn(false);
+    };
+
+    const handleLoginClick = () => {
+        setIsLoggedIn(true);
+    };
+
+    const [modalLogin, setModalLogin] = useState(false);
+    const [modalSignup, setModalSignup] = useState(false);
+
+    const showLogin = () => {
+        setModalLogin(true);
+    };
+
+    const cancelLoginModal= () => {
+        setModalLogin(false);
+    };
+
+    const showSignup = () => {
+        setModalSignup(true);
+    };
+
+    const cancelSignupModal= () => {
+        setModalSignup(false);
+    };
+
+    const switchToSignup = () => {
+        cancelLoginModal();
+        showSignup();
+    };
+
+    const switchToLogin = () => {
+        cancelSignupModal();
+        showLogin();
+    };
+
     return (
         <Disclosure as="nav" className="bg-gray-800 width-screen">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -29,30 +71,52 @@ const Navbar = () => {
                         </div>
                     </div>
 
-                    <Input type="search" placeholder="Tìm kiếm" className='w-[25%]'/>
+                    <Input type="search" placeholder="Tìm kiếm" suffix ={<SearchOutlined className='pl-2'/>} className='w-[25%]'/>
 
                     <div className="absolute inset-y-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        {/* <Button type="primary">Đăng nhập</Button> */}
-
-                        <BellOutlined className='text-white text-xl mr-2 cursor-pointer hover:scale-110'/>
-
-                        <div className="relative ml-3 group">
-                            <div className="relative flex rounded-full bg-gray-800 text-sm">
+                        {isLoggedIn ? (
+                            <>
+                            <BellOutlined className="text-white text-xl mr-2 cursor-pointer hover:scale-110" />
+                            <div className="relative ml-3 group">
+                                <div className="relative flex rounded-full bg-gray-800 text-sm">
                                 <span className="absolute -inset-1.5" />
                                 <span className="sr-only">Open user menu</span>
                                 <img
-                                className="h-8 w-8 rounded-full cursor-pointer"
-                                src={ava}
-                                alt=""
+                                    className="h-8 w-8 rounded-full cursor-pointer"
+                                    src={ava}
+                                    alt=""
                                 />
-                            </div>
-                            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden group-hover:block">
+                                </div>
+
+                                {/* User vs Hotel có trang Profile còn Admin có trang Admin */}
+                                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden group-hover:block">
                                 <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 active:font-bold" href="/profile">Profile</a>
-                                <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 active:font-bold" href="/">Logout</a>
+                                <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 active:font-bold" href="/admin">Admin</a>
+                                <button
+                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 active:font-bold"
+                                    onClick={handleLogoutClick}
+                                >
+                                    Logout
+                                </button>
+                                </div>
                             </div>
-                        </div>
+                            </>
+                        ) : (
+                            <Button type="primary" onClick={showLogin}> Đăng nhập </Button>
+                        )}
                     </div>
 
+                    <LoginModal visible={modalLogin} 
+                        onCancel={cancelLoginModal} 
+                        onLogin={() => {handleLoginClick(); cancelLoginModal();}}
+                        onSignup={switchToSignup}>
+                    </LoginModal>
+
+                    <SignupModal visible={modalSignup} 
+                        onCancel={cancelSignupModal} 
+                        onLogin={() => {handleLoginClick(); cancelLoginModal();}}
+                        onSignup={switchToLogin}>
+                    </SignupModal>
                 </div>
             </div>
 
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,195 @@
+import React, { useState } from 'react';
+import { Tabs, Table, Input, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons'
+import ava from '../assets/ava.png';
+import hotel from '../assets/hotel.png';
+import user from '../assets/user.png';
+
+const { TabPane } = Tabs;
+
+export default function Admin() {
+    const userColumns = [
+        {
+            title: "id",
+            dataIndex: "id",
+        },
+        {
+            title: "username",
+            dataIndex: "username",
+        },
+        {
+            title: "email",
+            dataIndex: "email",
+        },
+        {
+            title: "SĐT",
+            dataIndex: "phone number",
+        },
+        {
+            title: "Ngày tham gia",
+            dataIndex: "phone number",
+        },
+        {
+            title: "Khoá tài khoản",
+            dataIndex: "delete",
+        },
+    ]
+
+    const WaitlistColumns = [
+        {
+            title: "id",
+            dataIndex: "id",
+        },
+        {
+            title: "Tên khách sạn",
+            dataIndex: "name",
+        },
+        {
+            title: "username",
+            dataIndex: "username",
+        },
+        {
+            title: "email",
+            dataIndex: "email",
+        },
+        {
+            title: "SĐT",
+            dataIndex: "phone number",
+        },
+        {
+            title: "Ngày đăng ký",
+            dataIndex: "date",
+        },
+        {
+            title: "Phê duyệt",
+            dataIndex: "delete",
+        },
+    ]
+
+    const HotellistColumns = [
+        {
+            title: "id",
+            dataIndex: "id",
+        },
+        {
+            title: "Tên khách sạn",
+            dataIndex: "name",
+        },
+        {
+            title: "username",
+            dataIndex: "username",
+        },
+        {
+            title: "email",
+            dataIndex: "email",
+        },
+        {
+            title: "SĐT",
+            dataIndex: "phone number",
+        },
+        {
+            title: "Ngày duyệt",
+            dataIndex: "date",
+        },
+        {
+            title: "Khoá tài khoản",
+            dataIndex: "delete",
+        },
+    ]
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <div className="container mx-auto py-8 w-[80%]">
+                <div className="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+                    <div className="col-span-4 sm:col-span-3">
+                        <div className="bg-white shadow rounded-lg p-6 mb-3">
+                            <div className="flex flex-col items-center">
+                                <img src={ava} className="w-24 h-24 bg-gray-300 rounded-full mb-4" alt="Avatar"/>
+                                <h1 className="text-xl font-bold">Admin</h1>
+                            </div>
+                        </div>
+                        <div className="bg-white shadow rounded-lg p-6 mb-3 flex px-5 py-3 gap-3">
+                            <img src={hotel} width={70} height={70} />
+                            <div className="flex flex-col">
+                                <span className="text-2xl font-bold">xx</span>
+                                <span className="text-sm">Khách sạn</span>  
+                            </div>
+                        </div>
+                        <div className="bg-white shadow rounded-lg p-6 mb-3 flex px-5 py-3 gap-3">
+                            <img src={user} width={70} height={70} />
+                            <div className="flex flex-col">
+                                <span className="text-2xl font-bold">xx</span>
+                                <span className="text-sm">Khách hàng</span>  
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-span-4 sm:col-span-9">
+                        <div className="bg-white shadow rounded-lg p-6">
+                            <Tabs defaultActiveKey="1">
+                                <TabPane tab="Danh sách thành viên" key="1">
+                                    {/* Nội dung của Danh sách thành viên */}
+                                    <div className='flex justify-center'>
+                                        <Input
+                                            placeholder="username"
+                                            className="w-[40%] px-2 py-1 mb-3"
+                                            suffix={
+                                            <div className="rounded-l px-2 py-1 flex">
+                                                <SearchOutlined className="transition-all duration-300" />
+                                            </div>
+                                            }
+                                        />
+                                    </div>
+                                    
+                                    <Table scroll={{ x: 800 }} dataSource={[]} columns={userColumns} />
+                                </TabPane>
+                                <TabPane tab="Waitlist" key="2">
+                                    {/* Nội dung của Waitlist */}
+                                    <div className='flex justify-center'>
+                                        <Input
+                                            placeholder="Tên khách sạn"
+                                            className="w-[40%] px-2 py-1 mb-3"
+                                            suffix={
+                                            <div className="rounded-l px-2 py-1 flex">
+                                                <SearchOutlined className="transition-all duration-300" />
+                                            </div>
+                                            }
+                                        />
+                                    </div>
+                                    
+                                    <Table scroll={{ x: 800 }} dataSource={[]} columns={HotellistColumns} />
+                                </TabPane>
+                                <TabPane tab="Danh sách khách sạn" key="3">
+                                    {/* Nội dung của Danh sách khách sạn */}
+                                    <div className='flex justify-center'>
+                                        <Input
+                                            placeholder="Tên khách sạn"
+                                            className="w-[40%] px-2 py-1 mb-3"
+                                            suffix={
+                                            <div className="rounded-l px-2 py-1 flex">
+                                                <SearchOutlined className="transition-all duration-300" />
+                                            </div>
+                                            }
+                                        />
+                                    </div>
+                                    
+                                    <Table scroll={{ x: 800 }} dataSource={[]} columns={HotellistColumns} />
+                                </TabPane>
+                                <TabPane tab="Thông báo" key="4">
+                                    {/* Nội dung của Thông báo */}
+                                    <p>Tiêu đề</p>
+                                    <Input size="large" placeholder="Tiêu đề" className='mb-6' />
+                                    <p>Nội dung</p>
+                                    <Input.TextArea placeholder="Nhập thông báo của bạn ở đây..." rows={6} className='mb-6'/>
+                                    <div className='flex justify-center'>
+                                        <Button type='primary'>Gửi thông báo</Button>
+                                    </div>
+                                    
+                                </TabPane>
+                            </Tabs>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
