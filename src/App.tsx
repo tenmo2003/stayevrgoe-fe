e11fcfd9e4a2596c7c79c19914f7174644ec3e44
tenmo2003 <tenmo2003@gmail.com>
2024-05-25 16:06:42 +0700

    feat: added axios service

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 import Home from "./pages/home/Home";
-import Test from "./pages/test/Test";
 import Navbar from "./navs/NavBar";
 import Profile from "./pages/Profile";
 import HotelDetail from "./pages/detailHotel";
 import Payment from "./pages/Payment";
 import HotelList from "./pages/HotelList";
 import Admin from "./pages/Admin";
-
 
 function App() {
   return (
@@ -18,10 +16,9 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="test" element={<Test />} />
             <Route path="profile" element={<Profile />} />
-            <Route path="hoteldetail" element ={<HotelDetail/>}/>
-            <Route path="payment" element={<Payment/>}/>
+            <Route path="hoteldetail" element={<HotelDetail />} />
+            <Route path="payment" element={<Payment />} />
             <Route path="hotelList" element={<HotelList />} />
             <Route path="admin" element={<Admin />} />
           </Route>
